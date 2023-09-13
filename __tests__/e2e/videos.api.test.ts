860d95ae7bd1f7d40ac2cbc259f588b950a8686b
Ivan Malinovsky <imalinovsky@tradingview.com>
2023-09-13 22:24:08 +0300

    add more e2e tests

--- __tests__/e2e/videos.api.test.ts
+++ __tests__/e2e/videos.api.test.ts
@@ -1,10 +1,32 @@
 import request from 'supertest';
 import { app } from '../../src/index';
+import { getVideos } from '../../src/videos/videos-model';
 
 describe('/videos', () => {
-	it('should return 200', () => {
-		request(app)
-			.get('/videos')
-			.expect(200);
-	})
+	const initVideos = getVideos();
+	it('should return 200', async () => {
+		await request(app).get('/videos')
+			.expect(initVideos)
+			.expect(200)
+	});
+
+	it('should return 404', async () => {
+		await request(app).get('/videos/234524352345')
+			.expect(404)
+
+		await request(app).put('/videos/234524352345')
+			.expect(404)
+
+		 await request(app).delete('/videos/234524352345')
+			.expect(404)
+	});
+});
+
+describe('/testing', () => {
+	it('should delete all data and return 204', async () => {
+		await request(app)
+			.delete('/testing/all-data')
+			.expect({})
+			.expect(204)
+	});
 });
